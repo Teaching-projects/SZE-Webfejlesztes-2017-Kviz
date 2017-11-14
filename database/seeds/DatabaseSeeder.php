8c23b1f00c3ed4b7603bfb2858a84c689378ec5c
DESKTOP-BHLAJV9\Dominik <kollar.dominik0@gmail.com>
2017-11-14 19:21:03 +0100

    Alap adatbazis kerdesek

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Persistence\Model\Question;
+use App\Persistence\Model\Answer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(QuestionAnswerSeeder::class);
+    }
+}
+
+class QuestionAnswerSeeder extends Seeder{
+
+    /**
+     * Kérdések és válaszok feltöltése.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $question = new Question();
+        $question->question = "Mennyi 2+2?";
+        $question->save();
+
+        $question->answers()->saveMany([
+            $answer = new Answer(['answer' => '1', 'is_correct' => false]),
+            $answer = new Answer(['answer' => '2', 'is_correct' => false]),
+            $answer = new Answer(['answer' => '3', 'is_correct' => false]),
+            $answer = new Answer(['answer' => '4', 'is_correct' => true]),
+        ]);
+
+        $question = new Question();
+        $question->question = "Milyen színű az ég?";
+        $question->save();
+
+        $question->answers()->saveMany([
+            $answer = new Answer(['answer' => 'Zöld', 'is_correct' => false]),
+            $answer = new Answer(['answer' => 'Lila', 'is_correct' => false]),
+            $answer = new Answer(['answer' => 'Piros', 'is_correct' => false]),
+            $answer = new Answer(['answer' => 'Kék', 'is_correct' => true]),
+        ]);
+
+        $question = new Question();
+        $question->question = "Milyen színű az fű?";
+        $question->save();
+
+        $question->answers()->saveMany([
+            $answer = new Answer(['answer' => 'Zöld', 'is_correct' => true]),
+            $answer = new Answer(['answer' => 'Lila', 'is_correct' => false]),
+            $answer = new Answer(['answer' => 'Piros', 'is_correct' => false]),
+            $answer = new Answer(['answer' => 'Kék', 'is_correct' => false]),
+        ]);
+
     }
 }
